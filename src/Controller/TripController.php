c00b012fdbdbe1e7578ff166b58ab4074b424d43
Tanguy Leborgne <tanguy.leborgne2023@campus-eni.fr>
2023-10-31 16:59:53 +0100

    travail sur les trips

---
+++ src/Controller/TripController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trip;
+use App\Form\TripType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/trip')]
+class TripController extends AbstractController
+{
+    #[Route('/trip', name: 'app_trip')]
+    public function index(): Response
+    {
+        return $this->render('trip/index.html.twig', [
+            'controller_name' => 'TripController',
+        ]);
+    }
+
+    #[Route('/create', name: 'create_trip', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $trip = new Trip();
+        $tripForm = $this->createForm(TripType::class, $trip);
+
+        if($tripForm->isSubmitted() && $tripForm->isValid()){
+            $trip->setOrganizer($request->getUser());
+            $em->persist($trip);
+            $em->flush();
+
+            $this->addFlash('success', 'La sortie à bien été enregistrée !');
+
+            return $this->redirectToRoute('trip_details', ['id' => $trip->getId()]);
+        }
+
+        return $this->render('trip/tripCreate.html.twig', [
+            'tripForm' => $tripForm
+        ]);
+    }
+}
