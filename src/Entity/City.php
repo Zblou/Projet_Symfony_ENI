7509268762bab12a088ed173bab13e761d3c73a0
Teddy Privat <teddyprivat@hotmail.fr>
2023-10-31 11:46:13 +0100

    skusku

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\VilleRepository;
+use App\Repository\CityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
