c069659cf8456ba9572661deef9660b4bf535bb7
jlelouey2023 <julie@fffff.fr>
2023-11-08 10:14:58 +0100

    annulation sortie

---
+++ src/Form/AnnulationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trip;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnulationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('reasonCancellation', TextareaType::class, [
+                'label' => 'Reason:'
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trip::class,
+        ]);
+    }
+}
