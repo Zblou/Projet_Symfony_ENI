661a0ba872c78e946541830fcb151967dd5e2a33
Tanguy Leborgne <tanguy.leborgne2023@campus-eni.fr>
2023-11-08 16:17:55 +0100

    El famoso LOGO

--- src/Controller/TripFilterController.php
+++ src/Controller/TripFilterController.php
@@ -10,11 +10,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Constraints\Date;
 use function Symfony\Component\Clock\now;
 
 class TripFilterController extends AbstractController
 {
+    #[IsGranted('ROLE_USER')]
     #[Route('/displayAllTrips', name: 'display_all_updated', methods: ['GET', 'POST'])]
     public function filter(Request $request, TripRepository $tr, StateRepository $sr): Response
     {
