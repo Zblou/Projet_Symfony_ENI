b27505942982821f448f6b308d21bb255d321d5f
Teddy Privat <teddyprivat@hotmail.fr>
2023-11-08 12:14:28 +0100

    les trips qui ont lieu le jour de la consultation passent à l'état "In progress"

--- src/Controller/TripFilterController.php
+++ src/Controller/TripFilterController.php
@@ -4,23 +4,34 @@ namespace App\Controller;
 
 use App\Form\FilterType;
 use App\Form\Models\FilterModel;
+use App\Repository\StateRepository;
 use App\Repository\TripRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+use function Symfony\Component\Clock\now;
 
 class TripFilterController extends AbstractController
 {
     #[Route('/displayAllTrips', name: 'display_all_updated', methods: ['GET', 'POST'])]
-    public function filter(Request $request, TripRepository $tr): Response
+    public function filter(Request $request, TripRepository $tr, StateRepository $sr): Response
     {
         $trips = $tr->findAll();
 
         $filter = new FilterModel();
         $filterForm = $this->createForm(FilterType::class, $filter);
         $filterForm->handleRequest($request);
-
+        $date = now();
+        $dateUpdated =  $date->format('Y-m-d');
+        var_dump($dateUpdated);
+        foreach($trips as $trip){
+            $datesTrips = $trip->getDateStartTime()->format('Y-m-d');
+            if($datesTrips == $dateUpdated){
+                $trip->setState($sr->findOneBy(['name' => 'In progress']));
+            }
+        }
         if($filterForm->isSubmitted()){
             $trips = $tr->personnalizedSearch($filter, $this->getUser());
         }
