87ded5832fffdc081e18defe9aad5d0686d1cbe8
Teddy Privat <teddyprivat@hotmail.fr>
2023-11-09 09:59:59 +0100

    boutons d'annulation d'une sortie

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -12,9 +12,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class PlaceController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/addplace', name: 'add_place', methods: ['GET','POST'])]
     public function addPlace(Request $request, EntityManagerInterface $em): Response
     {
